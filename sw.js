08a5b7a09cad13cbbdf955b5bf62bb6c7b931b82
dgmacctg1999-cell <dgmacctg1999@gmail.com>
2025-11-14 16:01:07 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = "pwa-cache-v1";
+const FILES_TO_CACHE = [
+    "./",
+    "index.html",
+    "manifest.json",
+    "icon-192.png",
+    "icon-512.png"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(cached => cached || fetch(event.request))
+    );
+});
